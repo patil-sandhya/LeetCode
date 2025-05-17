17238fe8bbf63902df1f4c07fd6f17eeb64bd118
Sandhya Patil <117443062+patil-sandhya@users.noreply.github.com>
2025-05-17 19:29:17 +0530

    Time: 21 ms (5.07%), Space: 60.6 MB (8.58%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(mat) {
+    let r=mat.length
+    let c= mat[0].length
+      let zin = []
+    
+    for(let i=0; i<r; i++){
+        for(let j=0; j<c; j++){
+            if(mat[i][j] == 0){
+             zin.push([i,j])
+            }
+        }
+    }
+
+    for(let i=0; i<zin.length; i++){
+        changeMat(zin[i][0], zin[i][1])
+    }
+ function changeMat(row,col){
+     for(let k=0; k<r; k++){
+        mat[k][col] = 0;
+    }
+    //console.log(newmat)
+    for(let l=0; l<c; l++){
+        mat[row][l] = 0;
+    }
+    //console.log(newmat)
+ }
+//console.log(mat)
+    for(let i=0; i<r; i++){
+      let res = mat[i].join(" ")
+      console.log(res)
+    }
+return mat
+};
