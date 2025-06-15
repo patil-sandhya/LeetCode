b6ce678e15aedb323b14372b80f2f9cc918086a3
Sandhya Patil <117443062+patil-sandhya@users.noreply.github.com>
2025-06-15 21:29:58 +0530

    Time: 0 ms (100%), Space: 54.2 MB (56.57%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    strs.sort()
+    let first = strs[0]
+    let last = strs[strs.length-1]
+    let min = Math.min(first.length, last.length)
+    let i=0
+    let ans = ""
+    while(i<min && first[i] == last[i]){
+        ans+=first[i]
+        i++
+    }
+    return ans
+};
